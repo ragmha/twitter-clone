54e9bee4c603edbd888f7668af2f47cdeb17136a
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-31 02:24:32 +0300

    add basic crud

--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -1,14 +1,31 @@
 export default `
+scalar Date
+
+type Status {
+    message: String!
+}
+
 type Tweet {
-    _id: String,
-    text: String
-},
+    _id: ID!,
+    text: String!,
+    createdAt: Date!,
+    updatedAt: Date!
+}
 
 type Query {
+    getTweet(_id: ID!): Tweet
     getTweets: [Tweet]
 }
 
-schema {
-    query: Query
+type Mutation {
+    createTweet(text: String!): Tweet,
+    updateTweet(_id: ID!, text: String): Tweet,
+    deleteTweet(_id: ID!): Status
 }
+
+schema {
+    query: Query,
+    mutation: Mutation
+}
+
 `;
